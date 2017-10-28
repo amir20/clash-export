6ed29fb506cb38fa330a6c29c607f9c6dcdd2605
Amir Raminfar <findamir@gmail.com>
2017-10-27 20:17:36 -0700

    Updates scheduler to 75

--- app/scheduler.py
+++ app/scheduler.py
@@ -23,7 +23,7 @@ def update_clans():
     tags_to_fetch = list(all_tags - already_done)
 
     total = len(tags_to_fetch)
-    tags_to_fetch = tags_to_fetch[:50]
+    tags_to_fetch = tags_to_fetch[:75]
 
     logger.info(f"Fetching {len(tags_to_fetch)} of total {total} eligible items.")
 
