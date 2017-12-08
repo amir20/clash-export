fed8714b0eae6511f5745397c0916b3b32a8cc19
Amir Raminfar <findamir@gmail.com>
2017-12-07 16:23:53 -0800

    Adds home component

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,32 +1,10 @@
 import Vue from "vue";
-import ClanTag from "./components/ClanTag";
-import ClanCard from "./components/ClanCard";
-const STORAGE_KEY = "lastTag";
-const input = document.querySelector("input#tag");
-
-
-let savedTag = localStorage.getItem(STORAGE_KEY);
-
+import Home from "./components/Home"
 
 new Vue({
-  el: "#last-tag",  
+  el: "#home",  
   components: {
-    ClanCard
-  },
-  data: {
-    tag: savedTag
-  }
+    Home
+  }  
 });
-
-
-document.querySelector("form").addEventListener("submit", () => {
-  localStorage.setItem(STORAGE_KEY, input.value);
-});
-
-if (localStorage.getItem(STORAGE_KEY)) {
-  const tag = localStorage.getItem(STORAGE_KEY);
-  console.log(`Found last tag value [${tag}].`);
-  input.value = tag;
-}
-
 
