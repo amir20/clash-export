e1092c171d4d62a3035b7a9a79be7261a499ffe5
Amir Raminfar <findamir@gmail.com>
2017-12-13 08:42:20 -0800

    Fixes name

--- app/clash/clan_comparison.py
+++ app/clash/clan_comparison.py
@@ -6,12 +6,13 @@ from .transformer import transform_players
 def compare(now, then):
     now_df = to_data_frame(now)
     then_df = to_data_frame(then)
-    return (now_df['Total Gold Grab'] - then_df['Total Gold Grab']).mean()
+    delta = now_df.drop(columns='Tag') - then_df.drop(columns='Tag')
+    return delta['Total Gold Grab'].mean()
 
 
 def to_data_frame(clan):
     tf = transform_players(clan.players)
     df = pd.DataFrame(data=tf, columns=tf[0])
-    df.set_index('Name')
+    df = df.set_index('Name')
     df = df.iloc[1:]
     return df
