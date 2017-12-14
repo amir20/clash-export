d12ae8ae91a425fc6d5fef0ac9a938dd2687f582
Amir Raminfar <findamir@gmail.com>
2017-12-13 16:38:07 -0800

    Up the limit to 10 for updating

--- app/scheduler.py
+++ app/scheduler.py
@@ -23,7 +23,7 @@ def update_clans():
     tags_to_fetch = list(all_tags - already_done)
 
     total = len(tags_to_fetch)
-    tags_to_fetch = tags_to_fetch[:2]
+    tags_to_fetch = tags_to_fetch[:10]
 
     logger.info(f"Fetching {len(tags_to_fetch)} of total {total} eligible items.")
 
