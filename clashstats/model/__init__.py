dd8b799f0d8b59269106825fbb2a2ee083700396
Amir Raminfar <findamir@gmail.com>
2018-01-01 16:58:03 -0800

    Fixes model

--- clashstats/model/__init__.py
+++ clashstats/model/__init__.py
@@ -1,4 +1,4 @@
 from clashstats.model.clan import Clan
-from clashstats.model.clan_pre_calculated import ClanPreCalculated
+from clashstats.model.clan_pre_calculated import ClanPreCalculated, ClanDelta
 from clashstats.model.player import Player
 from clashstats.model.status import Status
