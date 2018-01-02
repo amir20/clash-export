13debdd21227d0105d9a9f8189b0b21a207f2475
Amir Raminfar <findamir@gmail.com>
2018-01-01 17:29:20 -0800

    Cleans up scheduler to use pre calc clans only

--- clashstats/scheduler.py
+++ clashstats/scheduler.py
@@ -1,8 +1,8 @@
 import logging
 import os
 import time
-
 from datetime import datetime, timedelta
+
 import schedule
 from clashstats.clash.calculation import update_calculations
 from clashstats.model import *
@@ -18,32 +18,28 @@ connect(db='clashstats', host=os.getenv('DB_HOST'), connect=False)
 logger = logging.getLogger(__name__)
 logging.getLogger("clashstats.clash.api").setLevel(logging.WARNING)
 
-all_tags = set(Clan.objects.distinct('tag'))
-
 
 def update_clans():
-    already_done = set(Clan.from_now(hours=12).distinct('tag'))
-    all_tags.update(already_done)
-    tags_to_fetch = list(all_tags - already_done)
+    twelve_hour_ago = datetime.now() - timedelta(hours=12)
+    query_set = ClanPreCalculated.objects(last_updated__lte=twelve_hour_ago)
+    total = query_set.count()
+    clans = query_set.limit(50)
 
-    total = len(tags_to_fetch)
-    tags_to_fetch = tags_to_fetch[:50]
+    logger.info(f"Fetching {len(clans)} of total {total} eligible clans.")
 
-    logger.info(f"Fetching {len(tags_to_fetch)} of total {total} eligible items.")
-
-    for tag in tags_to_fetch:
+    for c in clans:
         try:
-            logger.info(f"Updating clan {tag}.")
-            clan = Clan.fetch_and_save(tag)
-            update_calculations(clan)
+            logger.info(f"Updating clan {c.tag}.")
+            update_calculations(Clan.fetch_and_save(c.tag))
         except Exception:
             logger.exception(f"Error while fetching clan {tag}.")
             client.captureException()
 
     logger.debug(f"Done fetching clans.")
 
-    ratio_indexed = 100 * (len(Clan.from_now(hours=12).distinct('tag')) / len(all_tags))
-    total_clans = len(all_tags)
+    total_clans = ClanPreCalculated.objects.count()
+
+    ratio_indexed = 100 * (ClanPreCalculated.objects(last_updated__lte=twelve_hour_ago).count() / total_clans)
     Status.objects.update_one(
         set__ratio_indexed=ratio_indexed,
         set__total_clans=total_clans,
@@ -62,8 +58,7 @@ def update_clan_calculations():
     for tag in available_clan_tags:
         try:
             logger.info(f"Updating calculations for {tag}.")
-            clan = Clan.objects(tag=tag).first()
-            update_calculations(clan)
+            update_calculations(Clan.objects(tag=tag).first())
         except Exception:
             logger.exception(f"Error during updating clan calculation for {tag}.")
             client.captureException()
@@ -74,9 +69,11 @@ def delete_old_clans():
     logger.info(f"Deleted {deleted} clans that are older than 45 days.")
 
     tags = ClanPreCalculated.objects(members__lt=5).distinct('tag')
-    Clan.objects(tag__in=tags).delete()
+    deleted = Clan.objects(tag__in=tags).delete()
     logger.info(f"Deleted {deleted} clans with less than 5 members.")
-    [all_tags.discard(t) for t in tags]
+
+    deleted = ClanPreCalculated.objects(members=0).delete()
+    logger.info(f"Deleted {deleted} clans with 0 members.")
 
 
 def update_leaderboards():
@@ -92,13 +89,12 @@ def update_leaderboards():
                'avg_bh_level']
 
     for column in columns:
-        for clan in ClanPreCalculated.objects(members__gt=20).order_by(f"-{column}").limit(15):
+        for c in ClanPreCalculated.objects(members__gt=20).order_by(f"-{column}").limit(15):
             try:
-                logger.info(f"Updating {column} leaderboard clan {clan.tag}.")
-                clan = Clan.fetch_and_save(clan.tag)
-                update_calculations(clan)
+                logger.info(f"Updating {column} leaderboard clan {c.tag}.")
+                update_calculations(Clan.fetch_and_save(c.tag))
             except Exception:
-                logger.exception(f"Error while fetching leaderboard clan {tag}.")
+                logger.exception(f"Error while fetching leaderboard clan {c.tag}.")
                 client.captureException()
 
 
