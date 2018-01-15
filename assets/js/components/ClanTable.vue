401c2e98ce1f200cfbfe545cf62d86485133b0e1
Amir Raminfar <findamir@gmail.com>
2018-01-14 17:17:57 -0800

    Fixes typo

--- assets/js/components/ClanTable.vue
+++ assets/js/components/ClanTable.vue
@@ -50,7 +50,7 @@ export default {
     if (this.oldestDays < 3) {
       this.$snackbar.open({
         message:
-          "Hey Cheif! This is the first time I am seeing your clan. Try again in a few days to see historical data.",
+          "Hey stranger! This is the first time I am seeing your clan and so it will take a while to collect historical data. Come back again in a few days to see your updated stats.",
         type: "is-warning",
         position: "is-bottom-left",
         actionText: "Got it",
