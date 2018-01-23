1a526766e087387b112c004fded1f40e7ddf15c9
Amir Raminfar <findamir@gmail.com>
2018-01-22 20:13:59 -0800

    Fixes excel tab name

--- clashleaders/clash/excel.py
+++ clashleaders/clash/excel.py
@@ -1,15 +1,16 @@
 from io import BytesIO
 
-import xlsxwriter
+from xlsxwriter import Workbook
+from slugify import slugify
 
-from clashleaders.clash import transformer
+from clashleaders.clash.transformer import transform_players
 
 
 def to_stream(clan):
     stream = BytesIO()
-    data = transformer.transform_players(clan.players)
-    workbook = xlsxwriter.Workbook(stream)
-    worksheet = workbook.add_worksheet()
+    data = transform_players(clan.players)
+    workbook = Workbook(stream)
+    worksheet = workbook.add_worksheet(slugify(clan.name, capitalize=True, separator=' '))
     for row, data in enumerate(data):
         worksheet.write_row(row, 0, data)
     workbook.close()
