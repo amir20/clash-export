a1b65f5fdc6d6b5e21d780a4547cf041c2365e58
Amir Raminfar <findamir@gmail.com>
2018-01-23 20:53:18 -0800

    Uses bugsnag instead (#11)
    * Uses bugsnag instead
    
    * Adds git
    
    * Removes console
    
    * Removes api key from js
    
    * Fixes js error

--- assets/js/details-page.js
+++ assets/js/details-page.js
@@ -2,8 +2,12 @@ import Vue from "vue";
 import Buefy from "buefy";
 import ClanTable from "./components/ClanTable";
 import Tabs from "./components/Tabs";
+import bugsnag from "./bugsnag"
+
+bugsnag(Vue);
 
 Vue.use(Buefy, { defaultIconPack: "fa" });
+
 
 Vue.prototype.$bus = new Vue({})
 
