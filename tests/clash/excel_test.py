91406c99c43bfcf4b122b9e4ba1f0b25900d5928
Amir Raminfar <findamir@gmail.com>
2018-02-05 18:23:06 -0800

    Adds sheet names

--- tests/clash/excel_test.py
+++ tests/clash/excel_test.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import xlrd
 from clashleaders.clash.excel import to_stream
 
 
@@ -7,3 +8,10 @@ def test_excel_to_stream(clan_with_players, snapshot):
     df = pd.read_excel(stream)
 
     snapshot.assert_match(df.to_csv())
+
+
+def test_sheet_name(clan_with_players):
+    stream = to_stream(clan_with_players)
+    book = xlrd.open_workbook(file_contents=stream.read())
+
+    assert book.sheet_names() == ["#UGJPVJR"]
