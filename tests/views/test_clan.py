cf2b080aa721b276538226b8d353d4090e1c2647
Amir Raminfar <findamir@gmail.com>
2018-02-09 17:07:59 -0800

    Adds more tests

--- tests/views/test_clan.py
+++ tests/views/test_clan.py
@@ -1,9 +1,26 @@
+import clashleaders.views.clan
 
-def test_detail_page(client, mocker, clan_pre_calculated, first_clan_with_players, clan_table_html):
+
+def test_detail_page_oldest_days(client, mocker, clan_pre_calculated, first_clan_with_players):
+    patch_all(mocker, clan_pre_calculated, first_clan_with_players)
+    r = client.get('/clan/reddit-dynasty-ugjpvjr')
+
+    clashleaders.views.clan.render_template.assert_called_once_with('clan.html',
+                                                                    clan=clan_pre_calculated,
+                                                                    oldest_days=12,
+                                                                    players=mocker.ANY,
+                                                                    similar_clans=mocker.ANY,
+                                                                    description=mocker.ANY)
+
+
+def test_detail_page_status(client, mocker, clan_pre_calculated, first_clan_with_players):
+    patch_all(mocker, clan_pre_calculated, first_clan_with_players)
+    r = client.get('/clan/reddit-dynasty-ugjpvjr')
+    r.status_code == 200
+
+
+def patch_all(mocker, clan_pre_calculated, first_clan_with_players):
     mocker.patch('clashleaders.model.ClanPreCalculated.find_by_slug', return_value=clan_pre_calculated)
     mocker.patch('clashleaders.model.Clan.find_last_by_tag', return_value=first_clan_with_players)
     mocker.patch('clashleaders.views.clan.update_page_views')
-    r = client.get('/clan/reddit-dynasty-ugjpvjr')
-    assert r.status_code == 200
-
-    assert clan_table_html.encode() in r.data
+    mocker.patch('clashleaders.views.clan.render_template')
