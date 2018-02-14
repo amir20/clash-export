7ccfa2b7c8e1c5a322e19484247cd30570d71301
Amir Raminfar <findamir@gmail.com>
2018-02-14 12:14:22 -0800

    Add loop

--- clashleaders/clash/api.py
+++ clashleaders/clash/api.py
@@ -23,7 +23,7 @@ async def __fetch(url, session):
 def __get_all(urls):
     loop = asyncio.get_event_loop()
     jar = aiohttp.DummyCookieJar()
-    async with aiohttp.ClientSession(loop=, cookie_jar=jar, headers=HEADERS) as session:
+    async with aiohttp.ClientSession(loop=loop, cookie_jar=jar, headers=HEADERS) as session:
         futures = [__fetch(url, session) for url in urls]
         responses = loop.run_until_complete(asyncio.gather(*futures))
     loop.close()
