c4ea60f60d44e6da2d4be1e31c835df8035fdbcb
Amir Raminfar <findamir@gmail.com>
2018-02-19 09:17:44 -0800

    Adds hoverable and moves buttons around

--- assets/js/components/TableNav.vue
+++ assets/js/components/TableNav.vue
@@ -1,82 +1,85 @@
 <template>
-<div class="navbar-item">
-    <b-dropdown v-model="days" @change="changeDays">
-        <button class="button is-info" type="button" slot="trigger" :key="days">
-            <template v-if="days == 1">
-                <b-icon icon="hourglass" size="is-small" pack="far"></b-icon>
-                <span>Compare to Yesterday</span>
-            </template>
-            <template v-else-if="days == 7">
-                <b-icon icon="calendar-alt" size="is-small" pack="far"></b-icon>
-                <span>Compare to Last Week</span>
-            </template>
-            <template v-else-if="days == 30">
-                <b-icon icon="calendar" size="is-small" pack="far"></b-icon>
-                <span>Compare to Last Month</span>
-            </template>
-            <b-icon icon="chevron-down" size="is-small"></b-icon>
-        </button>
+<div class="navbar-start">
+    <div class="navbar-item">
+        <b-dropdown v-model="days" @change="changeDays" hoverable>
+            <button class="button is-info" type="button" slot="trigger" :key="days">
+                <template v-if="days == 1">
+                    <b-icon icon="hourglass" size="is-small" pack="far"></b-icon>
+                    <span>Compare to Yesterday</span>
+                </template>
+                <template v-else-if="days == 7">
+                    <b-icon icon="calendar-alt" size="is-small" pack="far"></b-icon>
+                    <span>Compare to Last Week</span>
+                </template>
+                <template v-else-if="days == 30">
+                    <b-icon icon="calendar" size="is-small" pack="far"></b-icon>
+                    <span>Compare to Last Month</span>
+                </template>
+                <b-icon icon="chevron-down" size="is-small"></b-icon>
+            </button>
 
-        <b-dropdown-item :value="1" v-if="totalDays > 0">
-            <div class="media">
-                <b-icon class="media-left" icon="hourglass" pack="far"></b-icon>
-                <div class="media-content">
-                    <h3>Yesterday</h3>
-                    <small>Compare your data to yesterday</small>
+            <b-dropdown-item :value="1" v-if="totalDays > 0">
+                <div class="media">
+                    <b-icon class="media-left" icon="hourglass" pack="far"></b-icon>
+                    <div class="media-content">
+                        <h3>Yesterday</h3>
+                        <small>Compare your data to yesterday</small>
+                    </div>
                 </div>
-            </div>
-        </b-dropdown-item>
-        <b-dropdown-item :value="7" v-if="totalDays > 2">
-            <div class="media">
-                <b-icon class="media-left" icon="calendar-alt" pack="far"></b-icon>
-                <div class="media-content">
-                    <h3>Last Week</h3>
-                    <small>Compare your data to a week ago</small>
+            </b-dropdown-item>
+            <b-dropdown-item :value="7" v-if="totalDays > 2">
+                <div class="media">
+                    <b-icon class="media-left" icon="calendar-alt" pack="far"></b-icon>
+                    <div class="media-content">
+                        <h3>Last Week</h3>
+                        <small>Compare your data to a week ago</small>
+                    </div>
                 </div>
-            </div>
-        </b-dropdown-item>
-        <b-dropdown-item :value="30" v-if="totalDays > 15">
-            <div class="media">
-                <b-icon class="media-left" icon="calendar" pack="far"></b-icon>
-                <div class="media-content">
-                    <h3>Last Month</h3>
-                    <small>Compare your data to last month</small>
+            </b-dropdown-item>
+            <b-dropdown-item :value="30" v-if="totalDays > 15">
+                <div class="media">
+                    <b-icon class="media-left" icon="calendar" pack="far"></b-icon>
+                    <div class="media-content">
+                        <h3>Last Month</h3>
+                        <small>Compare your data to last month</small>
+                    </div>
                 </div>
-            </div>
-        </b-dropdown-item>
-    </b-dropdown>
-
-    <b-dropdown v-model="sort" @change="changeSort">
-        <button class="button is-info" type="button" slot="trigger" :key="sort">
-            <template v-if="sort === 'delta'">
-                <b-icon icon="clock" size="is-small" pack="far"></b-icon>
-                <span>Sort by Stats Changed</span>
-            </template>
-            <template v-else>
-                <b-icon icon="chart-line" size="is-small"></b-icon>
-                <span>Sort by Most Recent</span>
-            </template>
-            <b-icon icon="chevron-down" size="is-small"></b-icon>
-        </button>
-        <b-dropdown-item value="value">
-            <div class="media">
-                <b-icon class="media-left" icon="chart-line"></b-icon>
-                <div class="media-content">
-                    <h3>Most Recent Stats</h3>
-                    <small>Compare your data using more recent stats</small>
+            </b-dropdown-item>
+        </b-dropdown>
+    </div>
+    <div class="navbar-item">
+        <b-dropdown v-model="sort" @change="changeSort" hoverable>
+            <button class="button is-info" type="button" slot="trigger" :key="sort">
+                <template v-if="sort === 'delta'">
+                    <b-icon icon="clock" size="is-small" pack="far"></b-icon>
+                    <span>Sort by Stats Changed</span>
+                </template>
+                <template v-else>
+                    <b-icon icon="chart-line" size="is-small"></b-icon>
+                    <span>Sort by Most Recent</span>
+                </template>
+                <b-icon icon="chevron-down" size="is-small"></b-icon>
+            </button>
+            <b-dropdown-item value="value">
+                <div class="media">
+                    <b-icon class="media-left" icon="chart-line"></b-icon>
+                    <div class="media-content">
+                        <h3>Most Recent Stats</h3>
+                        <small>Compare your data using more recent stats</small>
+                    </div>
                 </div>
-            </div>
-        </b-dropdown-item>
-        <b-dropdown-item value="delta">
-            <div class="media">
-                <b-icon class="media-left" icon="clock" pack="far"></b-icon>
-                <div class="media-content">
-                    <h3>Stats Changed</h3>
-                    <small>Sort the columns using the delta between today and previous stats</small>
+            </b-dropdown-item>
+            <b-dropdown-item value="delta">
+                <div class="media">
+                    <b-icon class="media-left" icon="clock" pack="far"></b-icon>
+                    <div class="media-content">
+                        <h3>Stats Changed</h3>
+                        <small>Sort the columns using the delta between today and previous stats</small>
+                    </div>
                 </div>
-            </div>
-        </b-dropdown-item>
-    </b-dropdown>
+            </b-dropdown-item>
+        </b-dropdown>
+    </div>
 </div>
 </template>
 
