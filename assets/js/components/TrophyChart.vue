dea84d7d2d37938068addb24f081d1566d0e71ed
Amir Raminfar <findamir@gmail.com>
2018-02-24 15:36:08 -0800

    Adds new chart

---
+++ assets/js/components/TrophyChart.vue
@@ -0,0 +1,50 @@
+<template>
+  <div ref="chart"></div>
+</template>
+
+<script>
+import Chartist from "chartist";
+import { bugsnagClient } from "../bugsnag";
+
+export default {
+  props: ["tag"],
+  data() {
+    return { data: null, chart: null };
+  },
+  async mounted() {
+    try {
+      this.data = await (await fetch(
+        `/clan/${this.tag.replace("#", "")}/trophies.json`
+      )).json();
+    } catch (e) {
+      console.error(e);
+      bugsnagClient.notify(e);
+    }
+
+    this.chart = new Chartist.Line(
+      this.$refs.chart,
+      {
+        labels: this.data.labels,
+        series: [
+          {
+            name: "series-1",
+            data: this.data.points
+          }
+        ]
+      },
+      {
+        fullWidth: true,
+        showArea: true,
+        showPoint: false,
+        showLine: false,
+        width: "100%",
+        height: "200px"
+      }
+    );
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
