99e27969398c723ffd8916b8a7340e36c0a03043
Amir Raminfar <findamir@gmail.com>
2018-02-26 06:40:29 -0800

    Moves mountd code to created

--- assets/js/components/TrophyChart.vue
+++ assets/js/components/TrophyChart.vue
@@ -11,49 +11,50 @@ export default {
   data() {
     return { data: null, chart: null };
   },
-  async mounted() {
+  async created() {
     try {
       this.data = await (await fetch(
         `/clan/${this.tag.replace("#", "")}/trophies.json`
       )).json();
+      this.$nextTick(() => {
+        this.chart = new Chartist.Line(
+          this.$refs.chart,
+          {
+            labels: this.data.labels,
+            series: [
+              {
+                name: "series-1",
+                data: this.data.points
+              }
+            ]
+          },
+          {
+            fullWidth: true,
+            showArea: true,
+            showPoint: false,
+            showLine: false,
+            width: "100%",
+            height: "180px",
+            axisX: {
+              showGrid: false,
+              labelInterpolationFnc(value, index) {
+                return index % 3 === 0
+                  ? new Date(value).toLocaleDateString()
+                  : null;
+              }
+            },
+            axisY: {
+              labelInterpolationFnc(value, index) {
+                return value.toLocaleString();
+              }
+            }
+          }
+        );
+      });
     } catch (e) {
       console.error(e);
       bugsnagClient.notify(e);
     }
-
-    this.chart = new Chartist.Line(
-      this.$refs.chart,
-      {
-        labels: this.data.labels,
-        series: [
-          {
-            name: "series-1",
-            data: this.data.points
-          }
-        ]
-      },
-      {
-        fullWidth: true,
-        showArea: true,
-        showPoint: false,
-        showLine: false,
-        width: "100%",
-        height: "180px",
-        axisX: {
-          showGrid: false,
-          labelInterpolationFnc(value, index) {
-            return index % 3 === 0
-              ? new Date(value).toLocaleDateString()
-              : null;
-          }
-        },
-        axisY: {
-          labelInterpolationFnc(value, index) {
-            return value.toLocaleString();
-          }
-        }
-      }
-    );
   }
 };
 </script>
