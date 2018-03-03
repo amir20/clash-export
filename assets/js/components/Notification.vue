0e8b22cd5bebfd4a63e6b0c927045e6de11161db
Amir Raminfar <findamir@gmail.com>
2018-03-03 13:16:37 -0800

    Adds file

---
+++ assets/js/components/Notification.vue
@@ -0,0 +1,24 @@
+<template>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["daysSpan"])
+  },
+  mounted() {
+    if (this.daysSpan < 3) {
+      this.$snackbar.open({
+        message:
+          "Hey stranger! This is the first time I am seeing your clan and so it will take a while to collect historical data. Come back again in a few days to see your updated stats.",
+        type: "is-warning",
+        position: "is-bottom-left",
+        actionText: "Got it",
+        duration: 20000
+      });
+    }
+  }
+};
+</script>
