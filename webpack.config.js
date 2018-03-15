5737d05a2d7b3d7a74f0d955ed1b40eb406a85a5
Amir Raminfar <findamir@gmail.com>
2018-03-15 08:13:35 -0700

    Adds css optimizer

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const ManifestPlugin = require("webpack-manifest-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   context: __dirname + "/assets",
@@ -122,6 +123,9 @@ if (process.env.NODE_ENV === "production") {
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
+    }),
+    new OptimizeCssAssetsPlugin({
+      cssProcessorOptions: { discardComments: { removeAll: true } },
     })
   ]);
 
