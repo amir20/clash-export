35f85732bdb0cb0056f4cca8950a94bb5b6b3ad3
Amir Raminfar <findamir@gmail.com>
2018-03-29 13:49:56 -0700

    Adds reddit links

--- clashleaders/views/verified.py
+++ clashleaders/views/verified.py
@@ -1,10 +1,21 @@
+import re
+
 from flask import render_template
 
-from clashleaders import app
+from clashleaders import app, cache
 from clashleaders.model import ClanPreCalculated
 
 
 @app.route("/verified/<tag>")
+@cache.cached(timeout=86400)
 def verified_clans(tag):
     clans = ClanPreCalculated.objects(verified_accounts=tag).order_by('-clanPoints')
+    for c in clans:
+        c.description = replace_description(c)
     return render_template('verified.html', clans=clans)
+
+
+def replace_description(clan):
+    return re.sub(r"((reddit.com)?(/r/\w+))", r'<a href="https://www.reddit.com\3/" target="_blank">\1</a>',
+                  clan.description,
+                  flags=re.IGNORECASE)
