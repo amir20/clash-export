8e5c7933e15951aa540894d2b865a372472f8ca7
Amir Raminfar <findamir@gmail.com>
2018-07-07 16:15:54 -0700

    Stop deleting small and inactive clans

--- clashleaders/batch/purge.py
+++ clashleaders/batch/purge.py
@@ -9,15 +9,6 @@ def delete_outdated():
     deleted = Clan.older_than(days=31).delete()
     logger.info(f"Deleted {deleted} clans that are older than 31 days.")
 
-    deleted = ClanPreCalculated.objects(members__lt=5).delete()
-    Clan.objects(members__lt=5).delete()
-    logger.info(f"Deleted {deleted} clans with less than 5 members.")
-
-    not_active_tags = [c.tag for c in ClanPreCalculated.objects(week_delta__total_donations=0, week_delta__total_attack_wins=0).only('tag')]
-    Clan.objects(tag__in=not_active_tags).delete()
-    not_active_deleted = ClanPreCalculated.objects(week_delta__total_donations=0, week_delta__total_attack_wins=0).delete()
-    logger.info(f"Deleted {not_active_deleted} clans with zero attacks.")
-
 
 def reset_stats():
     logger.info("Resetting page views...")
