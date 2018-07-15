3bc667a927ed2eeb5e209796ef494c52c0f06620
Amir Raminfar <findamir@gmail.com>
2018-07-15 10:03:54 -0700

    Adds active clans/members to status page

--- clashleaders/model/status.py
+++ clashleaders/model/status.py
@@ -9,7 +9,9 @@ from clashleaders.views.index import aggregate_by_country
 class Status(Document):
     last_updated = DateTimeField(default=datetime.now)
     total_clans = IntField(default=0)
+    total_active_clans = IntField(default=0)
     total_members = IntField(default=0)
+    total_active_members = IntField(default=0)
     total_countries = IntField(default=0)
     ratio_indexed = FloatField(default=0)
     popular_clans = ListField(ReferenceField(ClanPreCalculated))
@@ -29,8 +31,10 @@ class Status(Document):
         Status.objects.update_one(
             set__ratio_indexed=ratio_indexed,
             set__total_clans=total_clans,
+            set__total_active_clans=total_eligible_clans,
             set__last_updated=datetime.now(),
             set__total_members=ClanPreCalculated.objects.sum('members'),
+            set__total_active_members=ClanPreCalculated.objects(members__gte=5, week_delta__total_attack_wins__gt=0).sum('members'),
             set__total_countries=len(ClanPreCalculated.objects.distinct('location.countryCode')),
             set__popular_clans=ClanPreCalculated.objects.order_by('-page_views').limit(10),
             set__top_countries=aggregate_by_country("week_delta.avg_attack_wins"),
