5e6afe311c37e8fe04e73d86cda5c336ad8c21b3
Amir Raminfar <findamir@gmail.com>
2018-07-30 09:09:26 -0700

    Adds a distribution chart on home page

---
+++ assets/js/components/TrophyDistribution.vue
@@ -0,0 +1,49 @@
+<template>
+  <div ref="chart" class="trophy-distribution"></div>
+</template>
+
+<script>
+import Chartist from "chartist";
+import { bugsnagClient } from "../bugsnag";
+
+export default {
+  data() {
+    return { data: null, chart: null };
+  },
+  created() {
+    this.data = window.__DISTRIBUTION__;
+  },
+  mounted() {
+    this.chart = new Chartist.Bar(
+      this.$refs.chart,
+      {
+        labels: this.data.labels,
+        series: this.data.values
+      },
+      {
+        axisX: {
+          showLabel: false,
+          showGrid: false
+        },
+        axisY: {
+          showLabel: false,
+          showGrid: false
+        },
+        width: "100%",
+        fullWidth: true,
+        distributeSeries: true
+      }
+    );
+  }
+};
+</script>
+
+<style scoped>
+.trophy-distribution {
+  & /deep/ .ct-bar {
+    stroke-width: 6px;
+    stroke: #ff3860 !important;
+    stroke-linecap: round;
+  }
+}
+</style>
