a2e54099fbf0b772a37af81b2f99ef72412c08d1
Amir Raminfar <findamir@gmail.com>
2018-08-04 18:18:53 -0700

    Adds animation

--- assets/js/components/TrophyDistribution.vue
+++ assets/js/components/TrophyDistribution.vue
@@ -26,23 +26,42 @@ export default {
           showGrid: false
         },
         axisY: {
-          showLabel: false,
-          showGrid: false
+          showLabel: true,
+          showGrid: true
         },
         width: "100%",
         fullWidth: true,
         distributeSeries: true
       }
     );
+
+    this.chart.on("draw", function(data) {
+      if (data.type == "bar") {
+        data.element.animate({
+          y2: {
+            dur: "400ms",
+            from: data.y1,
+            to: data.y2,
+            easing: Chartist.Svg.Easing.easeOutQuint
+          }
+        });
+      }
+    });
+    this.chart.on("created", function() {
+      chart.detach(); // it will detach resize and media query listeners
+    });
   }
 };
 </script>
 
 <style scoped>
 .trophy-distribution {
+  width: 960px;
+  margin: 0 auto;
+
   & /deep/ .ct-bar {
     stroke-width: 6px;
-    stroke: #ff3860 !important;
+    stroke: #bbb;
     stroke-linecap: round;
   }
 }
