84858de1e7f067d12cca62f1393736df6b26c5f6
Amir Raminfar <findamir@gmail.com>
2018-08-05 21:14:46 -0700

    Index store (#425)
    * Removes docker-machine-deploy
    
    * Adds store to index page

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,15 +3,15 @@ import Buefy from "buefy";
 import Home from "./components/Home";
 import TrophyDistribution from "./components/TrophyDistribution";
 import bugsnag from "./bugsnag";
+import store from "./store/index-page";
 
 bugsnag(Vue);
 
 Vue.use(Buefy, { defaultIconPack: "fa" });
 
-Vue.prototype.$eventHub = new Vue();
-
 new Vue({
   el: "#app",
+  store,
   components: {
     Home,
     TrophyDistribution
