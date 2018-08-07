29d7adfad3269274e3aa2db7cd57e15b6d7afefa
Amir Raminfar <findamir@gmail.com>
2018-08-06 19:46:28 -0700

    Cleans up indexes

--- clashleaders/model/status.py
+++ clashleaders/model/status.py
@@ -26,15 +26,16 @@ class Status(Document):
     def update_status(cls):
         twelve_hour_ago = datetime.now() - timedelta(hours=12)
         total_clans = ClanPreCalculated.objects.count()
-        total_eligible_clans = ClanPreCalculated.objects(members__gte=5, week_delta__total_attack_wins__ne=0).count()
-        ratio_indexed = 100 * (ClanPreCalculated.objects(last_updated__gt=twelve_hour_ago, members__gte=5, week_delta__total_attack_wins__ne=0).count() / total_eligible_clans)
+        total_eligible_clans = ClanPreCalculated.active_clans().count()
+        not_indexed_clans = ClanPreCalculated.active_clans(twelve_hour_ago).count()
+        ratio_indexed = 100 * ((total_eligible_clans-not_indexed_clans) / total_eligible_clans)
         Status.objects.update_one(
             set__ratio_indexed=ratio_indexed,
             set__total_clans=total_clans,
             set__total_active_clans=total_eligible_clans,
             set__last_updated=datetime.now(),
             set__total_members=ClanPreCalculated.objects.sum('members'),
-            set__total_active_members=ClanPreCalculated.objects(members__gte=5, week_delta__total_attack_wins__ne=0).sum('members'),
+            set__total_active_members=ClanPreCalculated.active_clans().sum('members'),
             set__total_countries=len(ClanPreCalculated.objects.distinct('location.countryCode')),
             set__popular_clans=ClanPreCalculated.objects.order_by('-page_views').limit(10),
             set__top_countries=aggregate_by_country("week_delta.avg_attack_wins"),
