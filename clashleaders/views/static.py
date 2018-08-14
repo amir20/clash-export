023f5c84f9bf440f7c2ad198a2cfd90d2e595403
Amir Raminfar <findamir@gmail.com>
2018-08-14 12:03:23 -0700

    Adds caching

--- clashleaders/views/static.py
+++ clashleaders/views/static.py
@@ -2,15 +2,15 @@ import contentful
 
 from flask import render_template
 
-from clashleaders import app
-
+from clashleaders import app, cache
 
 client = contentful.Client('zmnodi6xws9d', '8017b7370aaf68e6caefc204d56fc6f2b3cee22adc907ebf0b4fbc40d7d98799')
 
 
 @app.context_processor
 def inject_changelog():
-    return dict(changelog=[{'title': e.title, 'summary': e.summary} for e in client.entries({'content_type': 'changelog'})])
+    logs = fetch_changelog()
+    return dict(changelog=[e['raw']['fields'] for e in logs])
 
 
 @app.route("/changelog")
@@ -31,3 +31,8 @@ def terms():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+
+@cache.cached(timeout=90, key_prefix='changelog')
+def fetch_changelog():
+    return client.entries({'content_type': 'changelog'})
