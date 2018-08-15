afbb40e8077ce13574aaa855396272b6e88d8d95
Amir Raminfar <findamir@gmail.com>
2018-08-15 12:55:45 -0700

    Updates (#437)
    * Adds update menu
    
    * Moves updates to vue and adds contentful lib
    
    * Changes to changlog and connects contentful
    
    * Adds caching
    
    * Fixes tests
    
    * Fixes javascript and adds filters
    
    * Adds changelog remember sticky
    
    * Renames class to changelog
    
    * Fixes size
    
    * Trims JSON for changelog
    
    * Fixes missing title
    
    * Adds published on

--- tests/views/test_clan.py
+++ tests/views/test_clan.py
@@ -25,5 +25,6 @@ def test_detail_page_status(client, mocker, clan_pre_calculated):
 def patch_all(mocker, clan_pre_calculated):
     mocker.patch('clashleaders.model.ClanPreCalculated.find_by_slug', return_value=clan_pre_calculated)
     mocker.patch('clashleaders.model.Status.get_instance', return_value=Status())
+    mocker.patch('clashleaders.views.static.fetch_changelog', return_value=[])
     mocker.patch('clashleaders.views.clan.update_page_views')
     mocker.patch('clashleaders.views.clan.render_template')
