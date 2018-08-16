174222d92631760c9b3cfca248a571256a64a62b
Amir Raminfar <findamir@gmail.com>
2018-08-16 08:03:44 -0700

    Player percentile (#443)
    * 1.3.10
    
    * Adds player percentile and refactors
    
    * Uses / instead
    
    * Uses ceil instead
    
    * Changes to last week instead of season

--- clashleaders/views/clan_player.py
+++ clashleaders/views/clan_player.py
@@ -1,14 +1,13 @@
 from flask import jsonify
 
 from clashleaders import app
+from clashleaders.clash.player_calculation import find_player_details
 from clashleaders.model import ClanPreCalculated
-from clashleaders.model.clan import prepend_hash
 
 
 @app.route("/clan/<clan_tag>/<player_tag>.json")
 def clan_player_json(clan_tag, player_tag):
     clan = ClanPreCalculated.find_by_tag(clan_tag)
-    player_tag = prepend_hash(player_tag)
-    player = next((p for p in clan.players if p['tag'] == player_tag), dict(tag=player_tag))
+    player = find_player_details(clan, player_tag)
 
     return jsonify(player)
