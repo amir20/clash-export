fb6016a0d32cc3c45aa672ce9a365c66d1af847f
Amir Raminfar <findamir@gmail.com>
2018-10-14 11:15:17 -0700

    Updates margins

--- assets/js/components/TrophyChart.vue
+++ assets/js/components/TrophyChart.vue
@@ -8,9 +8,6 @@ import { bugsnagClient } from "../bugsnag";
 
 export default {
   props: ["tag"],
-  data() {
-    return { data: null, chart: null };
-  },
   async created() {
     const json = await (await fetch(
       `/clan/${this.tag.replace("#", "")}/trophies.json`
@@ -22,27 +19,20 @@ export default {
         trophies: json.trophies[i],
         members: json.members[i]
       }));
-      this.data = data;
 
-      const margin = { top: 10, right: 20, bottom: 10, left: 50 };
+      const margin = { top: 10, right: 30, bottom: 10, left: 50 };
       const width = 960 - margin.left - margin.right;
-      const height = 180 - margin.top - margin.bottom;
+      const height = 140 - margin.top - margin.bottom;
 
       const x = d3.scaleTime().range([0, width]);
       const yLeft = d3.scaleLinear().range([height, 0]);
       const yRight = d3.scaleLinear().range([height, 0]);
 
-      const area = d3
+      const trophyArea = d3
         .area()
         .x(d => x(d.date))
         .y0(height)
         .y1(d => yLeft(d.trophies))
-        .curve(d3.curveMonotoneX);
-
-      const trophyLine = d3
-        .line()
-        .x(d => x(d.date))
-        .y(d => yLeft(d.trophies))
         .curve(d3.curveMonotoneX);
 
       // define the line 1
@@ -56,37 +46,20 @@ export default {
         .select(this.$refs.chart)
         .append("svg")
         .attr("preserveAspectRatio", "xMinYMin meet")
-        .attr("viewBox", "0 0 960 200")
+        .attr("viewBox", "0 0 960 150")
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-      x.domain(
-        d3.extent(data, function(d) {
-          return d.date;
-        })
-      );
-      yLeft.domain(
-        d3.extent(data, function(d) {
-          return d.trophies;
-        })
-      );
-      yRight.domain(
-        d3.extent(data, function(d) {
-          return d.members;
-        })
-      );
+      x.domain(d3.extent(data, d => d.date));
+      yLeft.domain(d3.extent(data, d => d.trophies));
+      const [yRightMin, yRightMax] = d3.extent(data, d => d.members);
+      yRight.domain([Math.min(yRightMax - 5, yRightMin), yRightMax]);
 
       svg
         .append("path")
         .data([data])
         .attr("class", "area")
-        .attr("d", area);
-
-      svg
-        .append("path")
-        .data([data])
-        .attr("class", "line")
-        .attr("d", trophyLine);
+        .attr("d", trophyArea);
 
       svg
         .append("path")
@@ -98,18 +71,23 @@ export default {
       svg
         .append("g")
         .attr("transform", "translate(0," + height + ")")
+        .attr("class", "axis x")
         .call(
           d3
             .axisBottom(x)
             .tickFormat(d3.timeFormat("%x"))
-            .ticks(d3.timeDay.filter(d => d3.timeDay.count(0, d) % 4 === 0))
+            .ticks(15)
         );
 
-      svg.append("g").call(d3.axisLeft(yLeft).ticks(4));
+      svg
+        .append("g")
+        .attr("class", "axis y")
+        .call(d3.axisLeft(yLeft).ticks(4));
 
       svg
         .append("g")
         .attr("transform", "translate( " + width + ", 0 )")
+        .attr("class", "axis y")
         .call(d3.axisRight(yRight).ticks(4));
     });
   }
@@ -118,20 +96,32 @@ export default {
 
 <style>
 .trophy-chart {
-  .line {
-    fill: none;
-    stroke: steelblue;
-    stroke-width: 2px;
-  }
-
   .members-line {
     fill: none;
-    stroke: rgb(255, 19, 2);
+    stroke: hsl(204, 86%, 53%);
     stroke-width: 1px;
+    stroke-dasharray: 4, 2;
   }
 
   .area {
-    fill: lightsteelblue;
+    fill: #d70206;
+    fill-opacity: 0.4;
+  }
+
+  .axis text {
+    color: rgba(0, 0, 0, 0.6);
+    font-family: "Titillium Web";
+  }
+
+  .axis.x .domain {
+    stroke: rgba(0, 0, 0, 0.25);
+  }
+  .axis.y .domain {
+    stroke: none;
+  }
+
+  .axis line {
+    stroke: none;
   }
 }
 </style>
