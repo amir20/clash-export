5b2fc577c0f86e748e880edd3a6160a01cc7aa5e
Amir Raminfar <findamir@gmail.com>
2018-10-24 12:36:51 -0700

    Adds player file

--- clashleaders/model/player.py
+++ clashleaders/model/player.py
@@ -1,12 +1,62 @@
-from mongoengine import DynamicDocument
+import json
+from codecs import decode, encode
+from mongoengine import DynamicDocument, BinaryField, signals, StringField
 
 
 class Player(DynamicDocument):
+    BINARY_FIELDS = ['achievements', 'clan', 'heroes', 'league', 'legendStatistics', 'spells', 'troops']
+
+    binary_bytes = BinaryField()
+    tag = StringField(required=True, unique=True)
+
     meta = {
         'indexes': [
-            'clan.name',
-            'clan.tag',
             'name',
             'tag'
         ]
     }
+
+    @classmethod
+    def upsert_player(cls, player_tag, **kwargs):
+        player = Player.objects(tag=player_tag).first()
+
+        if not player:
+            player = Player(**kwargs).save()
+        else:
+            # This is ugly but update() doesn't trigger pre_save
+            for key, value in kwargs.items():
+                setattr(player, key, value)
+            player.save()
+
+        return player
+
+    @classmethod
+    def post_init(cls, sender, document, **kwargs):
+        if document.binary_bytes:
+            data = decode_data(document.binary_bytes)
+
+            for f in cls.BINARY_FIELDS:
+                setattr(document, f, data[f])
+
+    @classmethod
+    def pre_save(cls, sender, document, **kwargs):
+        data = dict()
+
+        for f in cls.BINARY_FIELDS:
+            data[f] = getattr(document, f)
+            delattr(document, f)
+
+        document.binary_bytes = encode_data(data)
+
+
+signals.post_init.connect(Player.post_init, sender=Player)
+signals.pre_save.connect(Player.pre_save, sender=Player)
+
+
+def encode_data(map):
+    s = json.dumps(map)
+    return encode(s.encode('utf8'), 'zlib')
+
+
+def decode_data(b):
+    return json.loads(decode(b, 'zlib'))
