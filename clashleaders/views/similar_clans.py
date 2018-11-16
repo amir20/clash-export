72a7a27fdaae2999d7b80507f94da11946e8560e
Amir Raminfar <findamir@gmail.com>
2018-11-16 15:32:59 -0800

    Fixes similar clan averages

--- clashleaders/views/similar_clans.py
+++ clashleaders/views/similar_clans.py
@@ -6,16 +6,13 @@ from clashleaders.model import ClanPreCalculated
 
 @app.route("/clan/<tag>/similar/avg.json")
 def similar_clans_avg(tag):
-    ratio = int(request.args.get('daysAgo', 7)) / 7
+    days = int(request.args.get('daysAgo', 7))
+    key = {1: 'day_delta', 7: 'week_delta'}[days]
+
     cluster_label = ClanPreCalculated.find_by_tag(tag).cluster_label
-    gold = ClanPreCalculated.objects(cluster_label=cluster_label).average('week_delta.avg_gold_grab')
-    elixir = ClanPreCalculated.objects(cluster_label=cluster_label).average('week_delta.avg_elixir_grab')
-    de = ClanPreCalculated.objects(cluster_label=cluster_label).average('week_delta.avg_de_grab')
+    gold = ClanPreCalculated.objects(cluster_label=cluster_label).average(f"{key}.avg_gold_grab")
+    elixir = ClanPreCalculated.objects(cluster_label=cluster_label).average(f"{key}.avg_elixir_grab")
+    de = ClanPreCalculated.objects(cluster_label=cluster_label).average(f"{key}.avg_de_grab")
 
-    data = {
-        'gold_grab': gold * ratio,
-        'elixir_grab': elixir * ratio,
-        'de_grab': de * ratio,
-    }
-
+    data = dict(gold_grab=gold, elixir_grab=elixir, de_grab=de)
     return jsonify(data)
