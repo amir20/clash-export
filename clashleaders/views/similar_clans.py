017b28b2845b89e81ad34ed3560ef079cf0fffd8
Amir Raminfar <findamir@gmail.com>
2018-11-16 17:24:08 -0800

    Fixes cache to include query string and makes javascript faster

--- clashleaders/views/similar_clans.py
+++ clashleaders/views/similar_clans.py
@@ -1,10 +1,11 @@
 from flask import jsonify, request
 
-from clashleaders import app
+from clashleaders import app, cache
 from clashleaders.model import ClanPreCalculated
 
 
 @app.route("/clan/<tag>/similar/avg.json")
+@cache.cached(timeout=1200, query_string=True)
 def similar_clans_avg(tag):
     days = int(request.args.get('daysAgo', 7))
     key = {1: 'day_delta', 7: 'week_delta'}[days]
