b8df49fefb851b383e61ee053235725e92156326
Amir Raminfar <findamir@gmail.com>
2019-01-11 09:13:01 -0800

    Cleans up more calls

--- clashleaders/views/export.py
+++ clashleaders/views/export.py
@@ -5,19 +5,18 @@ from flask import render_template, send_file, request
 from mongoengine import DoesNotExist
 
 from clashleaders import app
-from clashleaders.model import ClanPreCalculated
-from clashleaders.views.clan import clan_near_days_ago
+from clashleaders.model import Clan
 
 
 @app.route("/clan/<slug>.xlsx")
 def clan_detail_xlsx(slug):
     try:
-        clan = ClanPreCalculated.find_by_slug(slug)
+        clan = Clan.find_by_slug(slug)
     except DoesNotExist:
         return render_template('error.html'), 404
     else:
-        clan = clan_near_days_ago(request.args.get('daysAgo', 0), clan.tag)
-        return send_file(to_stream(clan), attachment_filename=f"{clan.tag}.xlsx", as_attachment=True)
+        h = clan.historical_near_days_ago(request.args.get('daysAgo', 0))
+        return send_file(to_stream(h), attachment_filename=f"{clan.tag}.xlsx", as_attachment=True)
 
 
 def to_stream(clan):
