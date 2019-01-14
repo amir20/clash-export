021685919d321f2ed293f5d3a545e2b81154b2a7
Amir Raminfar <findamir@gmail.com>
2019-01-14 15:34:39 -0800

    Adds migrate

--- clashleaders/cli.py
+++ clashleaders/cli.py
@@ -3,7 +3,7 @@ import os
 
 from mongoengine import connect
 
-from clashleaders.model import Clan
+from clashleaders.model import Clan, ClanPreCalculated
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -23,3 +23,15 @@ def update_all_calculations():
             clan.update_calculations()
         except Exception:
             logger.exception("error while performing update_calculations")
+
+
+def migrate_pre_calculated():
+    total = ClanPreCalculated.objects.count()
+    i = 0
+    for clan in ClanPreCalculated.objects.only('tag').no_cache():
+        try:
+            i += 1
+            print(f"- {i}/{total}: {clan.tag}")
+            Clan.find_by_tag(clan.tag)
+        except Exception:
+            logger.exception("error while performing migrate_pre_calculated()")
