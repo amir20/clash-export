a6b1b0e728fdda21b1c4212139420223f44a7aab
Amir Raminfar <findamir@gmail.com>
2019-01-14 14:54:03 -0800

    Removes more of precalculated

--- clashleaders/model/status.py
+++ clashleaders/model/status.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
+
 from datetime import datetime, timedelta
 
 from mongoengine import DateTimeField, DictField, Document, FloatField, IntField, ListField, ReferenceField
 
-from clashleaders.model import ClanPreCalculated, Player, Clan
+from clashleaders.model import Player, Clan
 from clashleaders.views.index import aggregate_by_country
 
 
@@ -14,20 +16,20 @@ class Status(Document):
     total_active_members = IntField(default=0)
     total_countries = IntField(default=0)
     ratio_indexed = FloatField(default=0)
-    popular_clans = ListField(ReferenceField(ClanPreCalculated))
+    popular_clans = ListField(ReferenceField(Clan))
     top_countries = ListField(DictField())
-    reddit_clans = ListField(ReferenceField(ClanPreCalculated))
+    reddit_clans = ListField(ReferenceField(Clan))
 
     @classmethod
-    def get_instance(cls):
+    def get_instance(cls) -> Status:
         return Status.objects.first()
 
     @classmethod
     def update_status(cls):
         twelve_hour_ago = datetime.now() - timedelta(hours=12)
         total_clans = Clan.objects.count()
-        total_eligible_clans = ClanPreCalculated.active_clans().count()
-        not_indexed_clans = ClanPreCalculated.active_clans(twelve_hour_ago).count()
+        total_eligible_clans = Clan.active().count()
+        not_indexed_clans = Clan.active(twelve_hour_ago).count()
         ratio_indexed = 100 * ((total_eligible_clans - not_indexed_clans) / total_eligible_clans)
         Status.objects.upsert_one(
             set__ratio_indexed=ratio_indexed,
@@ -35,7 +37,7 @@ class Status(Document):
             set__total_active_clans=total_eligible_clans,
             set__last_updated=datetime.now(),
             set__total_members=Player.objects.count(),
-            set__total_active_members=ClanPreCalculated.active_clans().sum('members'),
+            set__total_active_members=Clan.active().sum('members'),
             set__total_countries=len(Clan.objects.distinct('location.countryCode')),
             set__popular_clans=Clan.objects.order_by('-page_views').limit(10),
             set__top_countries=aggregate_by_country("week_delta.avg_attack_wins"),
