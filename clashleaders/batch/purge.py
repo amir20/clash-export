715b5a9054951292f0019ca0fdf698531f026674
Amir Raminfar <findamir@gmail.com>
2019-02-16 16:01:07 -0800

    Adds delete outdated with historical data

--- clashleaders/batch/purge.py
+++ clashleaders/batch/purge.py
@@ -1,13 +1,15 @@
 import logging
+from datetime import timedelta, datetime
 
-from clashleaders.model import Clan
+from clashleaders.model import Clan, HistoricalClan, HistoricalPlayer
 
 logger = logging.getLogger(__name__)
 
 
 def delete_outdated():
     logger.info("Deleting outdated historical clans...")
-    pass  # todo
+    HistoricalClan.objects(created_on__lt=(datetime.now() - timedelta(days=31))).delete()
+    HistoricalPlayer.objects(created_on__lt=(datetime.now() - timedelta(days=31))).delete()
 
 
 def reset_stats():
