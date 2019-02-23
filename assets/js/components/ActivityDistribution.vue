0dd5f02df0944dabc4f3ae47c5d5881d928d6282
Amir Raminfar <findamir@gmail.com>
2019-02-23 15:51:41 -0800

    Adds legend

--- assets/js/components/ActivityDistribution.vue
+++ assets/js/components/ActivityDistribution.vue
@@ -6,6 +6,7 @@
 
 <script>
 import Chartist from "chartist";
+import "chartist-plugin-legend";
 import format from "date-fns/format";
 import fill from "lodash/fill";
 import times from "lodash/times";
@@ -26,16 +27,16 @@ export default {
     ...mapState(["loggedUserActivity", "playerActivity", "hasLoggedUser", "loading"]),
     data() {
       const dates = [];
-      const thisUser = [];
-      const loggedInUser = [];
+      const thisUser = { name: "This player", data: [], className: "player-activity" };
+      const loggedInUser = { name: "You", data: [], className: "logged-activity" };
       if (this.playerActivity[this.name]) {
         for (const [date, value] of Object.entries(this.playerActivity[this.name])) {
           dates.push(date);
-          thisUser.push(value);
+          thisUser.data.push(value);
           if (this.loggedUserActivity[this.name] && this.loggedUserActivity[this.name][date]) {
-            loggedInUser.push(this.loggedUserActivity[this.name][date]);
+            loggedInUser.data.push(this.loggedUserActivity[this.name][date]);
           } else if (this.hasLoggedUser) {
-            loggedInUser.push(0);
+            loggedInUser.data.push(0);
           }
         }
       }
@@ -59,6 +60,11 @@ export default {
           series: data.series
         },
         {
+          plugins: [
+            Chartist.plugins.legend({
+              classNames: ["player-activity", "logged-activity"]
+            })
+          ],
           axisX: {
             showLabel: true,
             showGrid: false,
@@ -72,7 +78,8 @@ export default {
           },
           axisY: {
             showLabel: true,
-            showGrid: false
+            showGrid: false,
+            labelInterpolationFnc: value => value.toLocaleString()
           },
           width: "100%",
           fullWidth: true,
@@ -121,20 +128,20 @@ const fakeData = {
     }
   }
 
-  & /deep/ .ct-series-a .ct-area {
+  & /deep/ .player-activity .ct-area {
     fill: #00d1b2;
     fill-opacity: 0.85;
   }
 
-  & /deep/ .ct-series-a .ct-line {
+  & /deep/ .player-activity .ct-line {
     stroke: none;
   }
 
-  & /deep/ .ct-series-b .ct-area {
+  & /deep/ .logged-activity .ct-area {
     fill: none;
   }
 
-  & /deep/ .ct-series-b .ct-line {
+  & /deep/ .logged-activity .ct-line {
     stroke: hsl(348, 100%, 61%);
     stroke-width: 2px;
     stroke-dasharray: 4, 2;
@@ -142,6 +149,30 @@ const fakeData = {
 
   & /deep/ .ct-chart-line .ct-label {
     white-space: nowrap;
+  }
+
+  & /deep/ .ct-legend {
+    font-size: 85%;
+    position: absolute;
+    right: 1em;
+
+    li {
+      display: inline;
+      padding-right: 5px;
+
+      &.player-activity:before,
+      &.ct-series-0:before {
+        background-color: #00d1b2;
+        border-color: #00d1b2;
+        left: 6px;
+      }
+
+      &.logged-activity:before {
+        background-color: hsl(348, 100%, 61%);
+        border-color: hsl(348, 100%, 61%);
+        left: 6px;
+      }
+    }
   }
 }
 </style>
