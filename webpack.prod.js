7f07bb3e42c9dde6ebebe6eb2a038bb87148b04e
Amir Raminfar <findamir@gmail.com>
2019-02-25 08:25:58 -0800

    Fixes sw for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,6 +6,9 @@ const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CompressionPlugin = require("compression-webpack-plugin");
 const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+const path = require("path");
+
+const assetsPath = path.resolve(__dirname, "./clashleaders");
 
 module.exports = merge(common, {
   mode: "production",
@@ -28,13 +31,12 @@ module.exports = merge(common, {
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("production")
     }),
-    new MiniCssExtractPlugin({
-      filename: "css/[name].[chunkhash].css"
-    }),
-    new CompressionPlugin({
-      test: /\.js$|\.css$|\.map$|\.svg$/
-    }),
-    new SWPrecacheWebpackPlugin()
+    new MiniCssExtractPlugin({ filename: "css/[name].[chunkhash].css" }),
+    new CompressionPlugin({ test: /\.js$|\.css$|\.map$|\.svg$/ }),
+    new SWPrecacheWebpackPlugin({
+      staticFileGlobsIgnorePatterns: [/\.map$/, /\.svg$/],
+      stripPrefix: assetsPath
+    })
   ],
   output: {
     filename: "js/[name].[chunkhash].js"
