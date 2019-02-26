efc513e9cce8707a181bbfd2d8ffc246fa48c477
Amir Raminfar <findamir@gmail.com>
2019-02-25 16:51:28 -0800

    Adds troops page

---
+++ assets/js/components/TroopAverageDistribution.vue
@@ -0,0 +1,55 @@
+<template>
+  <div ref="chart" class="troops-average"></div>
+</template>
+
+<script>
+import Chartist from "chartist";
+
+export default {
+  props: ["averages"],
+  mounted() {
+    new Chartist.Bar(
+      this.$refs.chart,
+      {
+        labels: this.data.levels,
+        series: this.data.series
+      },
+      {
+        axisX: {
+          showLabel: true,
+          showGrid: false
+        },
+        axisY: {
+          showLabel: true,
+          showGrid: true
+        },
+        width: "800px",
+        height: "300px",
+        stackBars: true
+      }
+    );
+  },
+  computed: {
+    data() {
+      const levels = [];
+      const averages = [];
+      const deltas = [];
+      const data = this.averages.sort(({ level: a }, { level: b }) => a - b);
+      for (const { level, max, avg } of data) {
+        levels.push(level);
+        averages.push(+avg);
+        deltas.push(+max - +avg);
+      }
+      return { levels, series: [averages, deltas] };
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.troops-average {
+  & /deep/ .ct-bar {
+    stroke-width: 50px;
+  }
+}
+</style>
