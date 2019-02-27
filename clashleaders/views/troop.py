f76df5f09bfa62fc2cbece33a3e209e069c28021
Amir Raminfar <findamir@gmail.com>
2019-02-27 09:56:38 -0800

    Fixes bugs for troops

--- clashleaders/views/troop.py
+++ clashleaders/views/troop.py
@@ -10,7 +10,7 @@ client = contentful.Client('zmnodi6xws9d', '8017b7370aaf68e6caefc204d56fc6f2b3ce
 @app.route("/troop/<slug>")
 def troop_html(slug):
     troop = fetch_troop(slug)
-    averages = AverageTroop.objects(name=troop.name)
+    averages = AverageTroop.objects(name=troop.name, is_builder_base=troop.base == 'Builder Base')
     avg_data = [{"level": a.th_level, "avg": a.avg, "max": a.max} for a in averages]
     return render_template('troop.html', troop=troop, averages=avg_data)
 
