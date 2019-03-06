fbd4eaa67cdb3683890f27219f8db7090dd74e74
Amir Raminfar <findamir@gmail.com>
2019-03-06 10:09:30 -0800

    Removes unused code

--- assets/js/components/Notification.vue
+++ assets/js/components/Notification.vue
@@ -5,10 +5,10 @@ import { mapState } from "vuex";
 
 export default {
   computed: {
-    ...mapState(["daysSpan"])
+    ...mapState(["clan"])
   },
   mounted() {
-    if (this.daysSpan < 3) {
+    if (this.clan.oldestDays < 3) {
       this.$snackbar.open({
         message:
           "Hey stranger! This is the first time I am seeing your clan. It will take a few days to collect historical data. Come back again to see your clan's activity.",
