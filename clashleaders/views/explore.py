d2afe7f0fbc411beec11b9e93aefadb3c73df363
Amir Raminfar <findamir@gmail.com>
2019-04-17 20:48:50 -0700

    Fixes titles

--- clashleaders/views/explore.py
+++ clashleaders/views/explore.py
@@ -6,13 +6,21 @@ from clashleaders.model import Clan
 ORDER_MAPPING = {
     "most-popular": "-page_views",
     "most-attacks": "-week_delta.avg_attack_wins",
-    "most-versus-trophies": "-week_delta.avg_versus_wins",
+    "most-versus-attacks": "-week_delta.avg_versus_wins",
     "most-donations": "-week_delta.avg_donations",
     "most-war-stars": "-week.avg_war_stars",
+}
+
+TITLE_MAPPING = {
+    "most-popular": "Most Popular",
+    "most-attacks": "Most Attacks",
+    "most-versus-attacks": "Most Versus Attacks",
+    "most-donations": "Most Donations",
+    "most-war-stars": "Most War Stars",
 }
 
 
 @app.route("/explore/<sort>")
 def explore_clans(sort):
     clans = Clan.objects(members__gt=20).order_by(ORDER_MAPPING[sort]).limit(50)
-    return render_template("explore.html", clans=clans, sort=sort)
+    return render_template("explore.html", clans=clans, sort=sort, title=TITLE_MAPPING[sort])
