96459cfe3b54f62e003abb3adce2addcd5886072
Amir Raminfar <findamir@gmail.com>
2019-04-21 11:08:01 -0700

    Tries to delay to animation

--- assets/js/components/ClanField.vue
+++ assets/js/components/ClanField.vue
@@ -45,7 +45,8 @@ export default {
       }
       requestAnimationFrame(animate);
       new TWEEN.Tween({ tweeningValue: startValue })
-        .to({ tweeningValue: endValue }, 800)
+        .to({ tweeningValue: endValue }, 1200)
+        .delay(Math.random() * 800)
         .easing(TWEEN.Easing.Quadratic.Out)
         .onUpdate(({ tweeningValue }) => (this.tweeningValue = Math.ceil(tweeningValue)))
         .start();
