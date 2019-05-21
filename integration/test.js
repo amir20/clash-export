5301de69f694b179c621e36644d0ef5b68c20c2c
Amir <amirraminfar@google.com>
2019-05-21 12:44:46 -0700

    Fixes tests and adds ints tests

---
+++ integration/test.js
@@ -0,0 +1,46 @@
+const puppeteer = require("puppeteer");
+
+const BASE = process.env.BASE;
+
+describe("jest-image-snapshot usage with an image received from puppeteer", () => {
+  let browser;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      executablePath: process.env.CHROME_EXE_PATH || "",
+      defaultViewport: { width: 1280, height: 1080 }
+    });
+  });
+
+  it("captures screenshot", async () => {
+    const page = await browser.newPage();
+    await page.goto(BASE, {
+      waitUntil: "networkidle0"
+    });
+    await page.waitFor(2000); // wait for animation on home page
+    const image = await page.screenshot({ fullPage: true });
+
+    expect(image).toMatchImageSnapshot({
+      failureThreshold: "0.05",
+      failureThresholdType: "percent"
+    });
+  });
+
+  // xit("captures screenshot reddit dynasty", async () => {
+  //   const page = await browser.newPage();
+  //   await page.goto("http://localhost:8000/clan/reddit-dynasty-ugjpvjr", {
+  //     waitUntil: "networkidle0"
+  //   });
+  //   const image = await page.screenshot({ fullPage: true });
+
+  //   expect(image).toMatchImageSnapshot({
+  //     failureThreshold: "0.05",
+  //     failureThresholdType: "percent"
+  //   });
+  // });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+});
