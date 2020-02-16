9e6535670b5a4c58f7aacfdef7769a90db7fcba5
Amir Raminfar <findamir@gmail.com>
2020-02-15 17:24:40 -0800

    Fixes excel bug for =

--- clashleaders/views/export.py
+++ clashleaders/views/export.py
@@ -21,7 +21,7 @@ def clan_detail_xlsx(slug):
 
 def to_stream(clan):
     stream = BytesIO()
-    writer = pd.ExcelWriter(stream, engine="xlsxwriter")
+    writer = pd.ExcelWriter(stream, engine="xlsxwriter", options={"strings_to_urls": False, "strings_to_formulas": False})
     clan.to_df(formatted=True).to_excel(writer, sheet_name=clan.tag)
     writer.close()
     stream.seek(0)
