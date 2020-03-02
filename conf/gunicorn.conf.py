3ef0a1284af2c4e3c8e53417bcbcac27d7e77e04
Amir Raminfar <findamir@gmail.com>
2020-03-02 15:12:23 -0800

    Adds threads

--- conf/gunicorn.conf.py
+++ conf/gunicorn.conf.py
@@ -1,7 +1,17 @@
 import multiprocessing
+import asyncio
+
 
 name = "clashleaders.com"
 bind = "unix:/tmp/gunicorn.sock"
-workers = multiprocessing.cpu_count() + 1
+workers = multiprocessing.cpu_count()
+threads = multiprocessing.cpu_count()
 worker_connections = 10000
-worker_class = "sync"
+worker_class = "gevent"
+
+
+def post_fork(server, worker):
+    from gevent import monkey
+
+    monkey.patch_all()
+    asyncio.set_event_loop(asyncio.new_event_loop())
