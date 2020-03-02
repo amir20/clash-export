ee89e648f566f9b449eb21975dd808f760deccc6
Amir Raminfar <findamir@gmail.com>
2020-03-02 13:21:48 -0800

    Reverts to sync

--- conf/gunicorn.conf.py
+++ conf/gunicorn.conf.py
@@ -2,6 +2,6 @@ import multiprocessing
 
 name = "clashleaders.com"
 bind = "unix:/tmp/gunicorn.sock"
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = multiprocessing.cpu_count() + 1
 worker_connections = 10000
-worker_class = "gevent"
+worker_class = "sync"
