6ebfdb1f032313ed65986cdda57ff1abc2ca03d5
Amir Raminfar <findamir@gmail.com>
2020-03-03 10:06:02 -0800

    Uses asyncio.run instead

--- conf/gunicorn.conf.py
+++ conf/gunicorn.conf.py
@@ -1,12 +1,9 @@
 import multiprocessing
-import asyncio
 
 
 name = "clashleaders.com"
 bind = "unix:/tmp/gunicorn.sock"
 workers = multiprocessing.cpu_count()
-threads = multiprocessing.cpu_count()
-worker_connections = 10000
 worker_class = "gevent"
 
 
@@ -14,4 +11,3 @@ def post_fork(server, worker):
     from gevent import monkey
 
     monkey.patch_all()
-    asyncio.set_event_loop(asyncio.new_event_loop())
