cbdcc92c1f264e9800f5e165f658a5160aa54569
Amir Raminfar <findamir@gmail.com>
2020-03-03 14:12:36 -0800

    Adds view cache

--- clashleaders/views/verified.py
+++ clashleaders/views/verified.py
@@ -6,13 +6,13 @@ from clashleaders.text.clan_description_processor import transform_description
 
 
 @app.route("/verified/<tag>")
+@cache.cached(600)
 def verified_clans(tag):
     clans = fetch_clans(tag)
 
     return render_template("verified.html", clans=clans)
 
 
-@cache.memoize(300)
 def fetch_clans(tag):
     clans = list(Clan.objects(verified_accounts=tag).order_by("-clanPoints"))
     for c in clans:
