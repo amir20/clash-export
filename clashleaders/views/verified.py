e4ab546612b6752e7219eff8847450f82cad17d9
Amir Raminfar <findamir@gmail.com>
2020-03-11 16:54:22 -0700

    Cleans up code

--- clashleaders/views/verified.py
+++ clashleaders/views/verified.py
@@ -8,14 +8,8 @@ from clashleaders.text.clan_description_processor import transform_description
 @app.route("/verified/<tag>")
 @cache.cached(600)
 def verified_clans(tag):
-    clans = fetch_clans(tag)
-
-    return render_template("verified.html", clans=clans)
-
-
-def fetch_clans(tag):
     clans = list(Clan.objects(verified_accounts=tag).order_by("-clanPoints"))
     for c in clans:
         c.description = transform_description(c.description)
 
-    return clans
+    return render_template("verified.html", clans=clans)
