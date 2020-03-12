30ace0bdfb666bc1054db89e895413ef2e2934ca
Amir Raminfar <findamir@gmail.com>
2020-03-12 10:12:11 -0700

    Fixes explore pages to have better results

--- clashleaders/views/country.py
+++ clashleaders/views/country.py
@@ -2,15 +2,15 @@ from flask import render_template
 
 from clashleaders import app, cache
 from clashleaders.model import Clan
-from clashleaders.text.clan_description_processor import transform_description
-from clashleaders.views.explore import ORDER_MAPPING
+from clashleaders.views.explore import ORDER_MAPPING, TITLE_MAPPING
 
 
 @app.route("/country/<code>", defaults={"sort": "trophies"})
 @app.route("/country/<code>/<sort>")
 @cache.cached(600)
 def country_clans(code, sort):
-    if clans := list(Clan.objects(location__countryCode=code.upper()).order_by(ORDER_MAPPING[sort]).limit(50)):
-        return render_template("country.html", clans=clans, name=clans[0].location["name"], sort=sort, code=code)
+    if clans := list(Clan.objects(location__countryCode=code.upper(), active_members__gte=10).order_by(ORDER_MAPPING[sort]).limit(50)):
+        title = f"{clans[0].location['name']}'s {TITLE_MAPPING[sort]}"
+        return render_template("country.html", clans=clans, title=title, sort=sort, code=code)
     else:
         return render_template("404.html"), 404
