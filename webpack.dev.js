465e04ad76f2cd8e44c7b7c89d88e5c7f4febd34
Amir Raminfar <findamir@gmail.com>
2021-01-20 13:05:36 -0800

    Updates redis and webpack for dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
-const path = require("path");
-
-const assetsPath = path.resolve(__dirname, "./clashleaders");
 
 module.exports = merge(common, {
   mode: "development",
@@ -12,10 +8,6 @@ module.exports = merge(common, {
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/[name].css",
-    }),
-    new SWPrecacheWebpackPlugin({
-      staticFileGlobsIgnorePatterns: [/\.map$/, /\.svg$/],
-      stripPrefix: assetsPath,
     }),
   ],
 });
