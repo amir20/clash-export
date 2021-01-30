97410617f475a05b935be83ecd864793fa1b4f05
Amir Raminfar <findamir@gmail.com>
2021-01-29 20:21:49 -0800

    Fixes tests

--- tests/views/clan_view_test.py
+++ tests/views/clan_view_test.py
@@ -4,21 +4,17 @@ import clashleaders.views.clan
 from clashleaders.model import Status
 
 
-def test_detail_page_oldest_days(client, mocker, clan):
+def test_detail(client, mocker, clan):
     patch_all(mocker, clan)
-    client.get("/clan/reddit-dynasty-ugjpvjr")
+    r = client.get("/clan/reddit-dynasty-ugjpvjr")
 
-    clashleaders.views.clan.render_template.assert_called_once_with(
-        "clan.html",
-        clan=clan,
-        initial_state=mocker.ANY,
-    )
+    assert b'<body class="clan_detail_page">' in r.data
 
 
 def test_detail_page_status(client, mocker, clan):
     patch_all(mocker, clan)
     r = client.get("/clan/reddit-dynasty-ugjpvjr")
-    assert r.status_code == 500  # Should fix this later
+    assert r.status_code == 200
 
 
 def patch_all(mocker, clan):
@@ -26,7 +22,6 @@ def patch_all(mocker, clan):
     mocker.patch("clashleaders.model.Status.instance", return_value=Status())
     mocker.patch("clashleaders.views.static.fetch_changelog", return_value=[])
     mocker.patch("clashleaders.views.manifest_map", return_value=defaultdict(lambda: "../tests/fixtures/dummy.txt"))
-    mocker.patch("clashleaders.views.clan.render_template")
     mocker.patch.object(clan, "similar_clans")
     mocker.patch.object(clan, "trophy_history")
     clan.similar_clans.return_value = [10, [clan]]
