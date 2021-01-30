23a5c50d453a25ead7e7aef9830daf9b85108e3f
Amir Raminfar <findamir@gmail.com>
2021-01-29 16:48:56 -0800

    Creates config file

--- clashleaders/__init__.py
+++ clashleaders/__init__.py
@@ -14,30 +14,26 @@ from mongoengine import connect
 from redis import Redis
 
 app = Flask(__name__, static_folder="_does_not_exists_", static_url_path="/static")
-app.secret_key = "2b2eaed61f28ea8ac252ace5e862bea1eb258c03f5669b3a"
-app.debug = bool(os.getenv("DEBUG", False))
+app.config.from_pyfile("config.py")
 
 # Template settings
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
 # RQ
-app.config["RQ_DASHBOARD_REDIS_HOST"] = "redis"
 app.register_blueprint(rq_dashboard.blueprint, url_prefix="/rq")
 
 # CSRF
 csrf = CSRFProtect()
 csrf.init_app(app)
 
-bugsnag.configure(
-    api_key=os.getenv("BUGSNAG_API_KEY"), project_root="/app", release_stage=os.getenv("STAGE", "development"), notify_release_stages=["production"]
-)
+bugsnag.configure(api_key=app.config["BUGSNAG_API_KEY"], project_root="/app", release_stage=app.env, notify_release_stages=["production"])
 handle_exceptions(app)
 
 logging.basicConfig(level=logging.INFO)
 
 # Cache settings
-cache_type = "null" if app.debug and not os.getenv("ENABLE_CACHE", False) else "redis"
+cache_type = "null" if app.env == "development" else "redis"
 cache = Cache(app, config={"CACHE_TYPE": cache_type, "CACHE_REDIS_HOST": cache_type})
 
 # Set connect to False for pre-forking to work
@@ -51,7 +47,7 @@ import clashleaders.views  # noqa
 import clashleaders.graphql.schema
 
 # GraphQL
-view_func = GraphQLView.as_view("graphql", schema=Schema(query=clashleaders.graphql.schema.Query), graphiql=True)
+view_func = GraphQLView.as_view("graphql", schema=Schema(query=clashleaders.graphql.schema.Query))
 app.add_url_rule("/graphql", view_func=view_func)
 
 
