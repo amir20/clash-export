b1eedd5e4814f498e8eefff3b42ded80d7005bd2
Amir Raminfar <findamir@gmail.com>
2021-08-10 10:15:53 -0700

    Adds CWL calculation with dfs

--- clashleaders/model/cwl_group.py
+++ clashleaders/model/cwl_group.py
@@ -10,7 +10,7 @@ from clashleaders.util import correct_tag, from_timestamp
 
 
 class CWLGroup(DynamicDocument):
-    round_wars = ListField(ReferenceField(CWLWar))
+    round_wars: List[CWLWar] = ListField(ReferenceField(CWLWar))
 
     meta = {
         "index_background": True,
@@ -21,6 +21,10 @@ class CWLGroup(DynamicDocument):
 
     def __repr__(self):
         return "<CWLGroup season={}>".format(self.season)
+
+    def to_df_for_clan(self, clan) -> List[CWLWar]:
+        cwl_wars = [war for war in self.round_wars if war.contains_clan(clan)]
+        return [war.to_df(clan) for war in cwl_wars]
 
     @classmethod
     def find_by_clan_and_season(cls, tag: str, season: str) -> Optional[CWLGroup]:
