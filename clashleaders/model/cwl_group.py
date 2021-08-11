6fb8e5bf67d158c635dab599b21b7d33ce957472
Amir Raminfar <findamir@gmail.com>
2021-08-11 13:26:03 -0700

    Adds clan CWL aggegations

--- clashleaders/model/cwl_group.py
+++ clashleaders/model/cwl_group.py
@@ -1,12 +1,17 @@
 from __future__ import annotations
+
+import pandas as pd
 from clashleaders.model.cwl_war import CWLWar
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 from mongoengine import DynamicDocument
-from datetime import datetime
 from typing import List
 
-from mongoengine.fields import BooleanField, ListField, ReferenceField, StringField
+from mongoengine.fields import ListField, ReferenceField, StringField
 from clashleaders.util import correct_tag, from_timestamp
+
+
+if TYPE_CHECKING:
+    from clashleaders.model.clan import Clan
 
 
 class CWLGroup(DynamicDocument):
@@ -22,9 +27,27 @@ class CWLGroup(DynamicDocument):
     def __repr__(self):
         return "<CWLGroup season={}>".format(self.season)
 
-    def to_df_for_clan(self, clan) -> List[CWLWar]:
-        cwl_wars = [war for war in self.round_wars if war.contains_clan(clan)]
-        return [war.to_df(clan) for war in cwl_wars]
+    def to_df_for_clan(self, clan: Clan) -> pd.DataFrame:
+        cwl_wars = (war for war in self.round_wars if war.contains_clan(clan))
+        tuples = ((war.startTime, war.to_df(clan)) for war in cwl_wars)
+        sorted_tuples = sorted(tuples, key=lambda tup: tup[0])
+        dfs = (tup[1] for tup in sorted_tuples)
+        data = {day: df for day, df in zip(range(1, 8), dfs)}
+        return pd.concat(data, axis=1)
+
+    def aggregate_stars_and_destruction(self, clan: Clan):
+        df = self.to_df_for_clan(clan)
+        stars = df.loc[:, (slice(None), "attack.stars")]
+        stars = stars.droplevel(level=1, axis=1)
+        stars.loc[:, "avg"] = stars.mean(axis=1)
+
+        destruction = df.loc[:, (slice(None), "attack.destructionPercentage")]
+        destruction = destruction.droplevel(level=1, axis=1)
+        destruction.loc[:, "avg"] = destruction.mean(axis=1)
+
+        name = df.droplevel(axis=1, level=0)["name"].dropna().iloc[:, 0]
+
+        return pd.concat([name, stars, destruction], axis=1, keys=["name", "stars", "destruction"])
 
     @classmethod
     def find_by_clan_and_season(cls, tag: str, season: str) -> Optional[CWLGroup]:
