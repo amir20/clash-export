e02b5224ee7a288cb17283259fac1d0670baa311
Amir Raminfar <findamir@gmail.com>
2021-09-24 11:41:40 -0700

    Fixes tests

--- tests/clash/api_test.py
+++ tests/clash/api_test.py
@@ -6,7 +6,11 @@ from clashleaders.clash import api
 
 def test_find_clan_bad():
     with aioresponses() as m:
-        m.get("https://api.clashofclans.com/v1/clans/%23ABC", status=500)
+        m.get(
+            "https://api.clashofclans.com/v1/clans/%23ABC",
+            status=500,
+            headers={"Cache-Control": "max-age=60"},
+        )
 
         with pytest.raises(api.ApiException):
             api.find_clan_by_tag("ABC")
@@ -14,7 +18,11 @@ def test_find_clan_bad():
 
 def test_find_clan_not_found():
     with aioresponses() as m:
-        m.get("https://api.clashofclans.com/v1/clans/%23ABC", status=404)
+        m.get(
+            "https://api.clashofclans.com/v1/clans/%23ABC",
+            status=404,
+            headers={"Cache-Control": "max-age=60"},
+        )
 
         with pytest.raises(api.ClanNotFound):
             api.find_clan_by_tag("ABC")
@@ -23,6 +31,11 @@ def test_find_clan_not_found():
 def test_find_clan_success():
     with aioresponses() as m:
         p = dict(tag="#ABC")
-        m.get("https://api.clashofclans.com/v1/clans/%23ABC", status=200, payload=p)
+        m.get(
+            "https://api.clashofclans.com/v1/clans/%23ABC",
+            status=200,
+            payload=p,
+            headers={"Cache-Control": "max-age=60"},
+        )
 
         assert api.find_clan_by_tag("ABC") == dict(tag="#ABC")
