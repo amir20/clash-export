5ebb6e4bb38673da58b24a339925e7ef5deb7891
Amir Raminfar <findamir@gmail.com>
2021-10-20 10:19:59 -0700

    Updates gunicorn with more memory

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,5 @@
 const { merge } = require("webpack-merge");
+const webpack = require("webpack");
 const common = require("./webpack.common.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
