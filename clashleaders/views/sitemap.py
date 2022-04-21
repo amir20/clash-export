e08c917b23f399139df9b8dfa2085a3c8fe4965d
Amir Raminfar <findamir@gmail.com>
2022-04-21 11:43:35 -0700

    Streams sitemap

--- clashleaders/views/sitemap.py
+++ clashleaders/views/sitemap.py
@@ -1,6 +1,6 @@
 from math import ceil
 
-from flask import make_response, render_template, url_for
+from flask import make_response, render_template, url_for, stream_with_context
 
 from clashleaders import app
 from clashleaders.model import Clan
@@ -23,18 +23,18 @@ def sitemap_index():
     return response
 
 
+def stream_template(template_name, **context):
+    app.update_template_context(context)
+    t = app.jinja_env.get_template(template_name)
+    rv = t.stream(context)
+    rv.enable_buffering(5)
+    return rv
+
+
 @app.route("/sitemap_<page>.xml")
 def sitemap(page):
     start = int(page) * TOTAL_PER_PAGE
     end = start + TOTAL_PER_PAGE
+    pages = ({"url": url_for("clan_detail_page", slug=clan.slug, _external=True)} for clan in Clan.objects[start:end].only("slug"))
 
-    pages = []
-
-    for clan in Clan.objects[start:end].only("slug"):
-        pages.append({"url": url_for("clan_detail_page", slug=clan.slug, _external=True)})
-
-    sitemap_xml = render_template("sitemap.xml", pages=pages)
-    response = make_response(sitemap_xml)
-    response.headers["Content-Type"] = "application/xml"
-
-    return response
+    return app.response_class(stream_with_context(stream_template("sitemap.xml", pages=pages)))
