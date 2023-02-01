f8dba09dd18caf86fa7dffa217466ad16323a5c0
Amir Raminfar <findamir@gmail.com>
2023-02-01 12:44:01 -0800

    Updates purge to batching

--- clashleaders/batch/purge.py
+++ clashleaders/batch/purge.py
@@ -9,19 +9,29 @@ logger = logging.getLogger(__name__)
 def delete_outdated():
     logger.info("Deleting outdated historical clans...")
     dt = datetime.now() - timedelta(days=7)
-    HistoricalClan.objects(created_on__lt=dt).delete()
-    HistoricalPlayer.objects(created_on__lt=dt).delete()
+    while deleted := HistoricalClan.objects(created_on__lt=dt).limit(1000).delete():
+        logging.info(f"Deleting {deleted} outdated historical clans...")
+        pass
+
+    while deleted := HistoricalPlayer.objects(created_on__lt=dt).limit(1000).delete():
+        logging.info(f"Deleting {deleted} outdated historical players...")
+        pass
+
     logger.info("Deleting 0 active_members clans...")
     Clan.objects(active_members=0).delete()
     HistoricalClan.objects(members=0).delete()
 
     logger.info("Deleting outdated wars...")
     dt = datetime.now() - timedelta(days=180)
-    ClanWar.objects(endTime__lt=dt).delete()
+    while deleted := ClanWar.objects(endTime__lt=dt).limit(1000).delete():
+        logging.info(f"Deleting {deleted} outdated wars...")
+        pass
 
     logger.info("Deleting outdated CWLs...")
     dt = datetime.now() - timedelta(days=90)
-    CWLWar.objects(endTime__lt=dt).delete()
+    while deleted := CWLWar.objects(endTime__lt=dt).limit(1000).delete():
+        logging.info(f"Deleting {deleted} outdated CWLs...")
+        pass
 
 
 def reset_stats():
